a63dc36c0ad2d2637c8712b52a2c2f3aa603ef65
Jesdin Raphael <jesdinraphael@gmail.com>
2018-07-22 22:44:50 +0530

    add New Task Button

--- src/com/jesdin/taskmanager/frames/MyTaskListFrame.java
+++ src/com/jesdin/taskmanager/frames/MyTaskListFrame.java
@@ -5,6 +5,11 @@ import javax.swing.*;
 public class MyTaskListFrame extends JFrame {
     public MyTaskListFrame() {
 
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+        var btnNewTask = new JButton("New Task");
+        add(btnNewTask);
+
         setTitle("My Tasklist");
         setSize(800, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
