6217adbf173b86983f3aa05c07413d8950ba7974
Jesdin Raphael <jesdinraphael@gmail.com>
2018-07-22 22:51:02 +0530

    add a Task Model

---
+++ src/com/jesdin/taskmanager/models/Task.java
@@ -0,0 +1,34 @@
+package com.jesdin.taskmanager.models;
+
+public class Task {
+
+    // private fields
+    private String title;
+    private boolean isHighPriority;
+    private boolean isCompleted;
+
+    // public properties
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isHighPriority() {
+        return isHighPriority;
+    }
+
+    public void setHighPriority(boolean highPriority) {
+        isHighPriority = highPriority;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+}
