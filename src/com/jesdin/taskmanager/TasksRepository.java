a2a5264937685e87db10bfb0cdce8bd11a29275f
Jesdin Raphael <jesdinraphael@gmail.com>
2018-08-24 22:37:43 +0530

    add Tasks Repository

---
+++ src/com/jesdin/taskmanager/TasksRepository.java
@@ -0,0 +1,44 @@
+package com.jesdin.taskmanager;
+
+import com.jesdin.taskmanager.models.Task;
+
+import java.util.ArrayList;
+
+public class TasksRepository {
+
+    public ArrayList<Task> get() {
+        return null;
+    }
+
+    public Task get(int id) {
+        return null;
+    }
+
+    public void add(Task task) {
+
+    }
+
+    public void edit(Task task) {
+
+    }
+
+    public void setCompleted(int id) {
+
+    }
+
+    public void setNotCompleted(int id) {
+
+    }
+
+
+//  constructor
+    public TasksRepository() {
+    }
+
+//    destructor
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+
+    }
+}
